b9c857e89d43552724febc6d5a28c60411d0873b
Shawn Patel <appdev.shawn@gmail.com>
2019-11-26 22:23:47 -0800

    Initial Commit

---
+++ Weather/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Weather
+//
+//  Created by Shawn Patel on 11/26/19.
+//  Copyright © 2019 Shawn Patel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
